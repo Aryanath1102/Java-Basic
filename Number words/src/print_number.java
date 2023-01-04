ff1cdf250b87259e28980b8641603763c102030a
Arya Nath <110094540+Aryanath1102@users.noreply.github.com>
2023-01-04 14:59:23 +0530

    Add files via upload

---
+++ Number words/src/print_number.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class print_number {
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your choice: ");
+        int x= sc.nextInt();
+        printnumberinword(x);
+    }
+    static void printnumberinword(int ch)
+    {
+        switch (ch)
+        {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            default:
+                System.out.println("other");
+                break;
+        }
+    }
+}
